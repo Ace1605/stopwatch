e5c1a0bc66e7466034e44169a12bec0bbbab10fc
Ace1605 <46580446+Ace1605@users.noreply.github.com>
2022-07-12 20:18:17 +0100

    update to supersized__

--- send.php
+++ send.php
@@ -7,8 +7,7 @@ echo "</pre>";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en">
-<head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<head>s
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 </head>
 <body>
