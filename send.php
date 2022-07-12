3568a1b1b7b0c20c1d8f133b9420c971d546a5ac
Ace1605 <46580446+Ace1605@users.noreply.github.com>
2022-07-12 19:17:24 +0100

    two jquery files

--- send.php
+++ send.php
@@ -7,9 +7,8 @@ echo "</pre>";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <head>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 </head>
 <body>
